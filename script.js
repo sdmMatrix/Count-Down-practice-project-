adee7b945afaf3bd6671e538276e4256b21e77b5
Charitha  Bandara <167965393+sdmMatrix@users.noreply.github.com>
2024-05-01 15:15:16 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const newYear = "1 january 2025";
+
+function countDown(){
+    const currentDate = new Date();
+    const newYearTotal = new Date(newYear);
+    const delay = (newYearTotal - currentDate) /1000;
+    
+    const date = Math.floor(delay /3600/24) ;
+    const hours = Math.floor(delay / 3600)% 24 ;
+    const minutes = Math.floor(delay /60) % 60;   
+    const second = Math.floor(delay)% 60;
+       
+    document.getElementById('secondCount').innerHTML = second;
+    document.getElementById('minutesCount').innerHTML = minutes;
+    document.getElementById('hoursCount').innerHTML = hours;
+    document.getElementById('daysCount').innerHTML = date;
+}
+
+setInterval(countDown,1000);
+
+
+
